738d5d64be46f702b674b6a0be49c1df93157e24
XAITYH <petrovskijpavel1@gmail.com>
2024-09-04 19:05:18 +0300

    checking if num1 and num2 are digits.

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-num1 = input("Введите первое число\n")
+num1 = input("Write the first number\n")
 
-num2 = input("Введите второе число\n")
+num2 = input("Write the second number\n")
 
-action = input("Введите действие из приведенных: *, **, /, +, -, %\n")
+action = input("Write one of the actions: *, **, /, +, -, %\n")
 
-
-match action:
-    case "*":
-        print(float(num1) * float(num2))
-    case "**":
-        print(float(num1) ** float(num2))
-    case "/":
-        print(float(num1) / float(num2))
-    case "+":
-        print(float(num1) + float(num2))
-    case "-":
-        print(float(num1) - float(num2))
-    case "%":
-        print(float(num1) % float(num2))
-    case _:
-        print("Something is wrong")
+if num1.isdigit() and num2.isdigit():
+    match action:
+        case "*":
+            print(float(num1) * float(num2))
+        case "**":
+            print(float(num1) ** float(num2))
+        case "/":
+            print(float(num1) / float(num2))
+        case "+":
+            print(float(num1) + float(num2))
+        case "-":
+            print(float(num1) - float(num2))
+        case "%":
+            print(float(num1) % float(num2))
+        case _:
+            print("Wrong action.")
+else:
+    print("You've made a typo or wrote a text instead of number.")
